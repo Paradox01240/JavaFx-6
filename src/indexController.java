8e5a818fe74d8ebf77581c319920e590cf992fce
paradox01240 <139707911+Paradox01240@users.noreply.github.com>
2025-02-19 21:40:13 +0530

    bulb change use checkbox in javaFx

---
+++ src/indexController.java
@@ -0,0 +1,23 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class indexController {
+    @FXML
+private CheckBox myCheckBox ;
+    @FXML
+    private Label label;
+    @FXML
+    private ImageView image;
+    public void change(){
+        if(myCheckBox.isSelected()){
+            label.setText("On");
+            image.setImage(new javafx.scene.image.Image("light-on.png"));
+        }
+        else{
+            label.setText("Off");
+            image.setImage(new javafx.scene.image.Image("light-off.png"));
+        }
+    }
+}
